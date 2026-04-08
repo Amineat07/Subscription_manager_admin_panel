b59bbc8ceb9aaa9206a1fb9997336bf541593f5b
Amineat07 <atattou1996@gmail.com>
2026-04-08 19:05:08 +0200

    adjust api route

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -16,7 +16,7 @@ function Login() {
     setLoading(true);
     try {
       const response = await axios.post(
-        `${import.meta.env.VITE_API_URL}/api/v1/login`,
+        `${import.meta.env.VITE_API_URL}/api/v1/auth/login`,
         { email, password },
         { withCredentials: true }
       );
